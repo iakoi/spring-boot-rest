b0b48db6155adcf4d6ca16707908c75942dfd725
jérémy Réchet <jeremyr@hotmail.fr>
2018-02-28 12:32:10 +0100

    setu up secrity en cours

---
+++ src/main/java/poe/spring/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package poe.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.antMatchers("/", "/signup", "/api***", "/css/**", "/js/**", "/images/**", "/webjars/**", "/poe-websocket").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+				.logout()
+				.permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER");
+	}
+}
