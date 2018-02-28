4ec8cfc1bd7aca9af6835e417427dbe789d71d68
jérémy Réchet <jeremyr@hotmail.fr>
2018-02-28 12:38:24 +0100

    login ok

--- src/main/java/poe/spring/WebSecurityConfig.java
+++ src/main/java/poe/spring/WebSecurityConfig.java
@@ -2,6 +2,10 @@ package poe.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -10,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
-				.antMatchers("/", "/signup", "/api***", "/css/**", "/js/**", "/images/**", "/webjars/**", "/poe-websocket").permitAll()
+				.antMatchers("/", "/signup", "/api***", "/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.formLogin()
@@ -25,6 +29,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 				.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER");
+				.withUser("a").password("a").roles("USER");
 	}
 }
